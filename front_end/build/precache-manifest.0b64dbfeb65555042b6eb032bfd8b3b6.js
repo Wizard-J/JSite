7290a41c9ca64d38075b263804e5f9cda1fae6cd
kaiwxin <xiongkai1223@gmail.com>
2019-09-06 17:08:51 +0800

    builded

---
+++ front_end/build/precache-manifest.0b64dbfeb65555042b6eb032bfd8b3b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf041a1ce4124dcd8ebd6fa868dcc12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6363d64c3b9070a0868d",
+    "url": "/static/css/2.88c1d975.chunk.css"
+  },
+  {
+    "revision": "6b0b812525413728b187",
+    "url": "/static/css/main.3d541643.chunk.css"
+  },
+  {
+    "revision": "6363d64c3b9070a0868d",
+    "url": "/static/js/2.c6354dce.chunk.js"
+  },
+  {
+    "revision": "6b0b812525413728b187",
+    "url": "/static/js/main.d6150ce5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa6cb952b86448c0fa1c3cd322f65392",
+    "url": "/static/media/sidebar-bg.fa6cb952.jpg"
+  }
+]);

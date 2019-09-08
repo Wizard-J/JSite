6fa56a46b9f6382c0a089ce9e3394b5c2c0a1737
kaiwxin <xiongkai1223@gmail.com>
2019-09-08 11:28:43 +0800

    managed to fix log

--- test/iotest.py
+++ test/iotest.py
@@ -1,10 +1,6 @@
 import os
 
 
-file =  open("./test.txt","a+")
-file.write("123\n")
-file.write("123\n")
-file.close()
-with open("./test.txt") as f:
+with open("./test.txt","r+") as f:
     for line in f.readlines():
         print(">>>",line)
